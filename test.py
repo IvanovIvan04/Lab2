6db5a9d8291af4979b6967884f78f721ef8a530c
Игнатьева Татьяна Александровна <ignateva_ta@emk.ru>
2023-03-31 19:25:27 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+def sum_series(x, n):
+    result = x
+    for i in range(1, n + 1):
+        if result < 0.999:
+            a = pow(-1, i) * pow(x, 2 * i + 1) / (2 * i + 1)
+            if a > 0.9999:
+                break
+            else:
+                result += a
+                print(a)
+                print(result)
+                print(i)
+        break
+
+    return round(result, 3)
+
+x = float(input())
+n = int(input())
+print(sum_series(x, n))
